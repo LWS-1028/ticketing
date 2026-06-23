522b4bd3602d0dd8e8df057ac4cbe06dbd58de7a
LWS <dldntjr4584@gmail.com>
2026-06-23 23:22:17 +0900

    add common api response

--- src/main/java/com/example/ticketing/health/api/HealthCheckController.java
+++ src/main/java/com/example/ticketing/health/api/HealthCheckController.java
@@ -1,5 +1,6 @@
 package com.example.ticketing.health.api;
 
+import com.example.ticketing.common.response.ApiResponse;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,7 +9,7 @@ import java.util.Map;
 @RestController
 public class HealthCheckController {
     @GetMapping("/api/health")
-    public Map<String, String> health() {
-        return Map.of("status", "OK");
+    public ApiResponse<Map<String, String>> health() {
+        return ApiResponse.success(Map.of("status", "OK"));
     }
 }
